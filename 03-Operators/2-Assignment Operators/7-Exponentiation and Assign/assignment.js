2a0d5925382684fda0d53d6947f9fbe2dd032e11
Praveen-kumar-cg <praveen.bishnoi.cg@gmail.com>
2026-09-16 18:26:44 +0530

    operator assignment published

---
+++ 03-Operators/2-Assignment Operators/7-Exponentiation and Assign/assignment.js
@@ -0,0 +1,15 @@
+//Exponentiation and Assign **=
+//Q1
+let side=10;
+side**=2;
+console.log(`The area of the garden is ${side} sqmeter`)
+
+//Q2
+let boxEdge=4;
+boxEdge**=3;
+console.log(`the volume of cube box is ${boxEdge}`)
+
+//Q3
+let sideFactor=3;
+sideFactor**=2;
+console.log(`the area growth of image is ${sideFactor}`)
