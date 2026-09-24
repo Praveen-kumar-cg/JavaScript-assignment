f89d114bc20bb4b0956663371f3978bc0923ad31
Praveen-kumar-cg <praveen.bishnoi.cg@gmail.com>
2026-09-24 18:25:57 +0530

    arithmetic new assignment published

--- 03-Operators/1-Arithmetc operators/assignment.js
+++ 03-Operators/1-Arithmetc operators/assignment.js
@@ -1,103 +1,300 @@
-//1. Addition +
-let collection1=15000;
-let collection2=12500;
-let totalCollection=collection1+collection2
-console.log(`The total collection is ${totalCollection}`)
+// //1. Addition +
+// let collection1=15000;
+// let collection2=12500;
+// let totalCollection=collection1+collection2
+// console.log(`The total collection is ${totalCollection}`)
 
-let readMorning=18;
-let readEvening=25;
-let totalPage=readEvening+readMorning;
-console.log(`the total page read is ${totalPage}`)
+// let readMorning=18;
+// let readEvening=25;
+// let totalPage=readEvening+readMorning;
+// console.log(`the total page read is ${totalPage}`)
 
-let soldMonday=125;
-let soldTuesday=178;
-let totalItemSold=soldMonday+soldTuesday;
-console.log(`the total item sold is ${totalItemSold}`)
+// let soldMonday=125;
+// let soldTuesday=178;
+// let totalItemSold=soldMonday+soldTuesday;
+// console.log(`the total item sold is ${totalItemSold}`)
 
+// //Q4
+// let a = "10";
+// let b = 5;
+// let result = a + b;
+// console.log(result);            //105
 
+// //Q5
+// let x = 5;
+// let y = "3";
+// let result = x + y;
+// console.log(result);           //53
 
-//2. Subtraction -
-let totalSeat=80;
-let occupiedSeat=53;
-let emptySeat=totalSeat-occupiedSeat;
-console.log(`The total empty seat are availabe is ${emptySeat}`)
+// //Q6
+// let p = "Hello";
+// let q = "World";
+// let result = p + " " + q;
+// console.log(result);          //Hello World
 
-let totalMarks=500;
-let marksLose=35;
-let finalMark=totalMarks-marksLose
-console.log(`The final marks is ${finalMark}`)
-
-let totalBox=2500;
-let sendBox=875;
-let remainingBox=totalBox-sendBox;
-console.log(`The remaining box in warehouse are ${remainingBox}`)
-
-
-
-//3. Multiplication *
-let cost=45;
-let notebookQuantity=8;
-let totalPrice=cost*notebookQuantity;
-console.log(`the total price of 8 notebook is ${totalPrice}`)
-
-let perHour=120;
-let hour=6;
-let totalBottels=perHour*hour;
-console.log(`The total bottel after 6 hour are ${totalBottels}`)
-
-let totalRow=7;
-let plant=15;
-let totalPlant=totalRow*plant;
-console.log(`the total plant in 7 row is ${totalPlant}`)
+// //Q7
+// let m = 0;
+// let n = false;
+// let result = m + n;
+// console.log(result);         //0
 
 
 
-//4. Division /
-let pencils=144;
-let totalStudent=12;
-let eachStudent=pencils/totalStudent;
-console.log(`The number of pencile each student get ${eachStudent}`)
-
-let travelDistance=360;
-let timeTaken=6;
-let average=travelDistance/timeTaken;
-console.log(`The average speed of the train is ${average}`)
-
-let totalMoney=72000;
-let depertments=9;
-let eachDepertment=totalMoney/depertments;
-console.log(`the moiney each depertment get is ${eachDepertment}`)
+// //Q8
+// let val1 = 100;
+// let val2 = "200";
+// let val3 = val1 + val2;
+// console.log(val3);             //100200   
 
 
-//5. Modulus %
-let totalStudents=53;
-let groupOf=5;
-let leftStudent=totalStudents%groupOf
-console.log(`the remenaning student are ${leftStudent}`)
 
-let totalCandies=128;
-let packOf=10;
-let leftCandies=totalCandies%packOf;
-console.log(`the remaning candies from the pack ${leftCandies}`)
+// //2. Subtraction -
+// let totalSeat=80;
+// let occupiedSeat=53;
+// let emptySeat=totalSeat-occupiedSeat;
+// console.log(`The total empty seat are availabe is ${emptySeat}`)
 
-let num = Number(prompt("Enter a number:"));
-if (num % 2 === 0) {
-  console.log("Even");
-} else {
-  console.log("Odd");
-}
+// let totalMarks=500;
+// let marksLose=35;
+// let finalMark=totalMarks-marksLose
+// console.log(`The final marks is ${finalMark}`)
 
-//6. Exponentiation **
-let side=6;
-let volume=side**3;
-console.log(`the volume of cube is ${volume} cm`)
+// let totalBox=2500;
+// let sendBox=875;
+// let remainingBox=totalBox-sendBox;
+// console.log(`The remaining box in warehouse are ${remainingBox}`)
 
-let bacteria=1;
-let hours=4;
-let bacteriaAfter=bacteria*(2**hours);
-console.log(`the bacteria after the 4 hours is ${bacteriaAfter}`)
+// //Q4
+// let a = "10";
+// let b = 3;
+// let result = a - b;
+// console.log(result);        //7
 
-let oneCell=9;
-let sides=4;
-let totalCells=oneCell*(sides**2);
-console.log(`The total number of cells are ${totalCells}`)
+// //Q5
+// let x = "20";
+// let y = "5";
+// let result = x - y;
+// console.log(result);       //15
+
+
+// //Q6
+// let p = "abc";
+// let q = 1;
+// let result = p - q;
+// console.log(result);        //NaN
+
+
+// //Q7
+// let m = 10;
+// let n = 0;
+// let result = m / n;
+// console.log(result);       //Infinity
+
+
+// //Q8
+// let val = 0 / 0;
+// console.log(val);           //NaN
+
+
+
+// //3. Multiplication *
+// let cost=45;
+// let notebookQuantity=8;
+// let totalPrice=cost*notebookQuantity;
+// console.log(`the total price of 8 notebook is ${totalPrice}`)
+
+// let perHour=120;
+// let hour=6;
+// let totalBottels=perHour*hour;
+// console.log(`The total bottel after 6 hour are ${totalBottels}`)
+
+// let totalRow=7;
+// let plant=15;
+// let totalPlant=totalRow*plant;
+// console.log(`the total plant in 7 row is ${totalPlant}`)
+
+// //Q4
+// let a = "5";
+// let b = 4;
+// let result = a * b;
+// console.log(result);               //20
+
+
+// //Q5
+// let x = "10";
+// let y = "2";
+// let result = x * y;
+// console.log(result);           //20
+
+
+// //Q6
+// let p = "hello";
+// let q = 2;
+// let result = p * q;
+// console.log(result);        //NaN
+
+
+
+// //Q7
+// let m = 5;
+// let n = "0";
+// let result = m * n;
+// console.log(result);       //0
+
+
+// //Q8
+// let val1 = 3;
+// let val2 = "4";
+// let val3 = val1 * val2;
+// console.log(val3);            //12
+
+
+
+// //4. Division /
+// let pencils=144;
+// let totalStudent=12;
+// let eachStudent=pencils/totalStudent;
+// console.log(`The number of pencile each student get ${eachStudent}`)
+
+// let travelDistance=360;
+// let timeTaken=6;
+// let average=travelDistance/timeTaken;
+// console.log(`The average speed of the train is ${average}`)
+
+// let totalMoney=72000;
+// let depertments=9;
+// let eachDepertment=totalMoney/depertments;
+// console.log(`the moiney each depertment get is ${eachDepertment}`)
+
+// //Q4
+// let a = "20";
+// let b = 4;
+// let result = a / b;
+// console.log(result);          //5
+
+
+// //Q5
+// let x = "100";
+// let y = "5";
+// let result = x / y;
+// console.log(result);           //20
+
+
+// //Q6
+// let p = 10;
+// let q = 0;
+// let result = p / q;
+// console.log(result);            //Infinity
+
+
+
+// //Q7
+// let m = -10;
+// let n = 0;
+// let result = m / n;
+// console.log(result);          //-infinity
+
+// //Q8
+// let val = 0 / 0;
+// console.log(val);             //NaN
+
+
+// //5. Modulus %
+// let totalStudents=53;
+// let groupOf=5;
+// let leftStudent=totalStudents%groupOf
+// console.log(`the remenaning student are ${leftStudent}`)
+
+// let totalCandies=128;
+// let packOf=10;
+// let leftCandies=totalCandies%packOf;
+// console.log(`the remaning candies from the pack ${leftCandies}`)
+
+// let num = Number(prompt("Enter a number:"));
+// if (num % 2 === 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+// //Q6
+// let a = 10;
+// let b = 0;
+// let result = a % b;
+// console.log(result);           //NaN
+
+
+// //Q7
+// let x = 0;
+// let y = 5;
+// let result = x % y;
+// console.log(result);          //0
+
+
+// //Q8
+// let p = -10;
+// let q = 3;
+// let result = p % q;
+// console.log(result);       //-1
+
+
+// //Q9
+// let m = 10;
+// let n = -3;
+// let result = m % n;
+// console.log(result);            //1
+
+
+// //Q10
+// let val1 = -10;
+// let val2 = -3;
+// let val3 = val1% val2;
+// console.log(val3);                //-1
+
+
+// //6. Exponentiation **
+// let side=6;
+// let volume=side**3;
+// console.log(`the volume of cube is ${volume} cm`)
+
+// let bacteria=1;
+// let hours=4;
+// let bacteriaAfter=bacteria*(2**hours);
+// console.log(`the bacteria after the 4 hours is ${bacteriaAfter}`)
+
+// let oneCell=9;
+// let sides=4;
+// let totalCells=oneCell*(sides**2);
+// console.log(`The total number of cells are ${totalCells}`)
+
+
+// //Q6
+// let side = -2;
+// let area = side ** 2;
+// console.log(area);                  //4
+
+
+// //Q7
+// let base = 2;
+// let power = -1;
+// let result = base ** power;
+// console.log(result);               //0.5
+
+
+// //Q8
+// let val = 2 ** -2;
+// console.log(val);                //0.25
+
+
+// //Q9
+// let x = 3;
+// let y = 2;
+// let z = x ** y;
+// console.log(z);               //9
+
+
+// //Q10
+// let a = 10;
+// let b = 0;
+// let result = a ** b;
+// console.log(result);               //1
